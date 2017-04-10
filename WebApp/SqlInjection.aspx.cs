8cb62ee2d44f06ec36540bad38df05b8ed2eb6a8
QA SDL <pt.sdl.qa@gmail.com>
2017-04-10 13:27:29 +0700

    Init

---
+++ WebApp/SqlInjection.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Web.UI;
+using System.Data.Entity.Core.EntityClient;
+
+namespace WebApp
+{
+    public partial class SqlInjection : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Request.Params["command"] != null)
+            {
+                var con = new EntityConnection("connection");
+                con.GetSchema(Request.Params["command"]);
+            }
+        }
+    }
+}
